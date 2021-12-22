edffe7333bcc1768fba4b3642af06936a7fa5ec8
Arek Cichocki <arexspeed95@gmail.com>
2021-12-22 11:25:29 +0100

    update, del

--- restblog/models/Post.php
+++ restblog/models/Post.php
@@ -97,4 +97,53 @@ class Post {
 
     return false;
   }
+
+  //Update
+  public function update() {
+    // Create query
+    $query = 'UPDATE' . $this->table . ' SET title = :title, body = :body, author = :author, category_id = :category_id WHERE id = :id';
+
+    // Prepare statement
+    $stmt = $this->conn->prepare($query);
+
+    // Clean data
+    $this->title = htmlspecialchars(strip_tags($this->title));
+    $this->body = htmlspecialchars(strip_tags($this->body));
+    $this->author = htmlspecialchars(strip_tags($this->author));
+    $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+    $this->id = htmlspecialchars(strip_tags($this->id));
+
+    // Bind data
+    $stmt->bindParam(':title', $this->title);
+    $stmt->bindParam(':body', $this->body);
+    $stmt->bindParam(':author', $this->author);
+    $stmt->bindParam(':category_id', $this->category_id);
+    $stmt->bindParam(':id', $this->id);
+
+    // Execute query
+    if($stmt->execute()) {
+      return true;
+    }
+
+    // Print error if something goes wrong
+    printf("Error: %s.\n", $stmt->error);
+
+    return false;
+  }
+
+  // Delete Post
+  public function delete() {
+    $query = 'DELETE FROM ' . $this->table . ' WHERE id = :id';
+    $stmt = $this->conn->prepare($query);
+    $this->id = htmlspecialchars(strip_tags($this->id));
+    $stmt->bindParam(':id', $this->id);
+    if($stmt->execute()) {
+      return true;
+    }
+
+    // Print error if something goes wrong
+    printf("Error: %s.\n", $stmt->error);
+
+    return false;
+  }
 }
