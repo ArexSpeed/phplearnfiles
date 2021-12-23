18042abfc90fbbf84dcc158ab78cc56de6f8e9f7
Arek Cichocki <arexspeed95@gmail.com>
2021-12-23 14:38:29 +0100

    user session

--- chatapp/users.php
+++ chatapp/users.php
@@ -1,22 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Realtime ChatApp</title>
-  <link rel="stylesheet" href="style.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-</head>
+<?php
+  session_start();
+  if(!isset($_SESSION['unique_id'])){
+    header("location: login.php");
+  }
+  include_once "header.php";
+?>
 <body>
   <div class="wrapper">
     <section class="users"> 
       <header>
+        <?php
+          include_once "php/config.php";
+          $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
+          if(mysqli_num_rows($sql) > 0){
+            $row = mysqli_fetch_assoc($sql);
+          }
+        ?>
         <div class="content">
-          <img src="img.png" alt="" />
+          <img src="php/images/<?= $row['image']; ?>" alt="" />
           <div class="details">
-            <span>Arex Speed</span>
-            <p>Active Now</p>
+            <span><?= $row['fname'] . " " . $row['lname'] ?></span>
+            <p><?= $row['status']; ?></p>
           </div>
         </div>
         <a href="#" class="logout">Logout</a>
