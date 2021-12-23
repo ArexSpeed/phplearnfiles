9cfba0642ed58299b0ad37e8b311a434e61a47e0
Arek Cichocki <arexspeed95@gmail.com>
2021-12-23 14:13:44 +0100

    login check

--- chatapp/login.php
+++ chatapp/login.php
@@ -13,24 +13,25 @@
     <section class="form login"> 
       <header>Realtime Chat App</header>
       <form action="#">
-        <div class="error-txt">This is an error message</div>
+        <div class="error-txt"></div>
         <div class="field input">
           <label for="">Email</label>
-          <input type="text" placeholder="Enter your email">
+          <input type="text" name="email" placeholder="Enter your email">
         </div>
         <div class="field input">
           <label for="">Password</label>
-          <input type="password" placeholder="Enter your password">
+          <input type="password" name="password" placeholder="Enter your password">
           <i class="fas fa-eye"></i>
         </div>
         <div class="field button">
           <input type="submit" value="Continue to Chat">
         </div>
       </form>
-      <div class="link">Not yet signed up? <a href="#">Signup now</a></div>
+      <div class="link">Not yet signed up? <a href="signup.php">Signup now</a></div>
     </section>
   </div>
 
   <script src="js/pass-show-hide.js"></script>
+  <script src="js/login.js"></script>
 </body>
 </html>
