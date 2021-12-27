26cf071c70d77bcf5a170de378126d5d4690b123
Arek Cichocki <arexspeed95@gmail.com>
2021-12-27 13:35:52 +0100

    customize/dynamic title, menus

--- wptheme_fcc/footer.php
+++ wptheme_fcc/footer.php
@@ -1,4 +1,11 @@
-    <!-- Bootstrap Javascript -->    
+<footer class="footer text-center py-2 theme-bg-dark">
+
+<p class="copyright"><a href="https://youtube.com/FollowAndrew">FollowAndrew</a></p>
+
+</footer>
+
+</div>
+ <!-- Bootstrap Javascript -->    
     <?php
       wp_footer();
     ?>      
@@ -6,7 +13,6 @@
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 	<script src="js/main.js"></script> -->
-
 
 </body>
 </html> 
--- wptheme_fcc/functions.php
+++ wptheme_fcc/functions.php
@@ -1,5 +1,24 @@
 <?php
 
+function followandrew_theme_support(){
+  //Add dynamic title
+  add_theme_support('title-tag');
+  //title get from custom name sets in wp theme dashboard
+}
+
+add_action('after_theme_setup', 'followandrew_theme_support');
+
+function followandrew_menus(){
+  $locations = array(
+    'primary' => "Desktop Primary Left Sidebar",
+    'footer' => "Footer Menu Items"
+  );
+
+  register_nav_menus($locations);
+}
+
+add_action('init', 'followandrew_menus'); //add action call as hook
+
 function followandrew_register_styles(){
   //add for let front-page load correct style.css
   $version = wp_get_theme()->get('Version');
@@ -17,5 +36,8 @@ function followandrew_register_scripts(){
   wp_enqueue_script('followandrew-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js", array(), '4.4.1', true);
   wp_enqueue_script('followandrew-main', get_template_directory_uri() . "/assets/js/main.js", array(), '1.0', true);
 }
+
+add_action('wp_enqueue_scripts', 'followandrew_register_scripts');
+
 
 ?>
