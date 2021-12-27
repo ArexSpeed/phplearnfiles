3b8ec71691b199436f2363e118eb331df8717665
Arek Cichocki <arexspeed95@gmail.com>
2021-12-27 16:02:06 +0100

    content archive/posts

--- wptheme_fcc/functions.php
+++ wptheme_fcc/functions.php
@@ -41,5 +41,32 @@ function followandrew_register_scripts(){
 
 add_action('wp_enqueue_scripts', 'followandrew_register_scripts');
 
+function followandrew_widget_areas(){
+  register_sidebar(
+    array(
+      'before_title' => '<h2>',
+      'after_title' => '</h2>',
+      'before_widget' => '<ul class="social-list list-inline py-3 mx-auto">',
+      'after_widget' => '</ul>',
+      'name' => 'Sidebar Area',
+      'id' => 'sidebar-1',
+      'description' => 'Sidebar Widget Area'
+    )
+  );
+
+  register_sidebar(
+    array(
+      'before_title' => '',
+      'after_title' => '',
+      'before_widget' => '',
+      'after_widget' => '',
+      'name' => 'Footer Area',
+      'id' => 'footer-1',
+      'description' => 'Footer Widget Area'
+    )
+  );
+}
+
+add_action('widgets_init', 'followandrew_widget_area');
 
 ?>
---
+++ wptheme_fcc/readme.txt
@@ -0,0 +1,3 @@
+All folder is only a part of all wordpress file which is necessary to work this code.
+Templete should be in wp_content->themes (add own theme directory)
+https://www.youtube.com/watch?v=-h7gOJbIpmo
