274388786787d2bd1d4dc945cef2bee44e4bc6ae
Arek Cichocki <arexspeed95@gmail.com>
2021-12-30 12:20:12 +0100

    pagination

--- wpwoo/wp-content/themes/woocommerce-theme/archive.php
+++ wpwoo/wp-content/themes/woocommerce-theme/archive.php
@@ -23,6 +23,20 @@
           <?php the_excerpt(); ?>
         </div>
       <?php endwhile; else: endif; ?>
+
+      <div class="pagination">
+        <?php
+          global $wp_query;
+          $big = 999999999;
+
+          echo paginate_links( array(
+            'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+            'format' => '?paged=%#%',
+            'current' => max(1, get_query_var('paged')),
+            'total' => $wp_query->max_num_pages
+          ) );
+        ?>
+      </div>
     </div>
   </div>
 
